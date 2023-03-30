ac79f9c5b64b3879dccca0c2a948951cc4702f86
Connor Norton <connorn314@gmail.com>
2023-03-30 15:19:37 -0700

    Initial Commit

---
+++ lock-pickr/lock_pickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  lock_pickrApp.swift
+//  lock-pickr
+//
+//  Created by Connor Norton on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct lock_pickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
